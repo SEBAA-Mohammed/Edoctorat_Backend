f67ac8372cef0cbc4ee9c0fd7cb0cc0f3e33fbce
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-15 23:04:00 -1200

    candidat model

---
+++ src/main/java/com/estf/edoctorat/models/CandidatModel.java
@@ -0,0 +1,40 @@
+package com.estf.edoctorat.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity()
+@Data
+public class CandidatModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String cne;
+    private String cni;
+    private String nomCandidatAr;
+    private String prenomCandidatAr;
+    private String adresseAr;
+    private String adresse;
+    private String sexe;
+    private String villeDeNaissance;
+    private String villeDeNaissanceAr;
+    private String ville;
+    private Date dateDeNaissance;
+    private String typeDeHandicape;
+    private String academie;
+    private String telCandidat;
+    private String pathCv;
+    private String pathPhoto;
+    private int etatDossier;
+    private String situation_familiale;
+    private String pay_id;
+    private String user_id;
+    private int fonctionaire;
+}
