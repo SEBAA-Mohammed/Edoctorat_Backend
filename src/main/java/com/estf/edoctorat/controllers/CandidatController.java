0e47b8d092c252d850d0f1ed256bdf3db954ce96
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-15 23:27:38 -1200

    add : index method in candidat controller

--- src/main/java/com/estf/edoctorat/controllers/CandidatController.java
+++ src/main/java/com/estf/edoctorat/controllers/CandidatController.java
@@ -1,16 +1,23 @@
 package com.estf.edoctorat.controllers;
 
+import com.estf.edoctorat.models.CandidatModel;
+import com.estf.edoctorat.services.CandidatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/candidats")
 public class CandidatController {
 
+    @Autowired
+    private CandidatService candidatService;
     @GetMapping()
-    public String candidats() {
-        return "Candidats";
+    public List<CandidatModel> index() {
+        return candidatService.getCandidats();
     }
 }
