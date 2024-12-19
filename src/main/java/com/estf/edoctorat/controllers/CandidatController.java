3a926c23a5a63082ef71ab83d447cf33db7ada78
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-19 09:53:57 -1200

    fix: get by id and name are now working

--- src/main/java/com/estf/edoctorat/controllers/CandidatController.java
+++ src/main/java/com/estf/edoctorat/controllers/CandidatController.java
@@ -11,22 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 @RequestMapping("/candidats")
 public class CandidatController {
 
     @Autowired
     private CandidatService candidatService;
+
     @GetMapping()
     public List<CandidatModel> index() {
         return candidatService.getCandidats();
     }
+
     @GetMapping("/candidatById/{id}")
     public Optional<CandidatModel> getById(@PathVariable Long id) {
         return candidatService.getCandidatById(id);
     }
-    @GetMapping("/candidatByName{name}")
+
+    @GetMapping("/candidatByName/{name}")
     public List<CandidatModel> getByName(@PathVariable String name) {
         return candidatService.getCandidatByName(name);
     }
