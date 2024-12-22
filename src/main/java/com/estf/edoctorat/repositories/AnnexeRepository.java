4d1d690dcbc4122b79808b1111e0dad818c31cc4
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-22 10:22:30 -1200

    fix : add repository annotation to diplome and annexe repository

--- src/main/java/com/estf/edoctorat/repositories/AnnexeRepository.java
+++ src/main/java/com/estf/edoctorat/repositories/AnnexeRepository.java
@@ -2,6 +2,8 @@ package com.estf.edoctorat.repositories;
 
 import com.estf.edoctorat.models.AnnexeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnnexeRepository extends JpaRepository<AnnexeModel, Long> {
 }
--- src/main/java/com/estf/edoctorat/repositories/DiplomeRepository.java
+++ src/main/java/com/estf/edoctorat/repositories/DiplomeRepository.java
@@ -2,6 +2,8 @@ package com.estf.edoctorat.repositories;
 
 import com.estf.edoctorat.models.DiplomeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiplomeRepository extends JpaRepository<DiplomeModel, Long> {
 }
